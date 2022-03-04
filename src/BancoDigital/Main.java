26a49bf6649e9210a79ab4eb070eb4714bc580eb
usuario <joaorodrigo@id.uff.br>
2022-03-03 21:31:49 -0300

    Banco Digital Java

---
+++ src/BancoDigital/Main.java
@@ -0,0 +1,16 @@
+package BancoDigital;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Conta cc = new ContaCorrente();
+        Conta poupanca = new ContaPoupanca();
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        cc.imprimirInfosComuns();
+
+    }
+}
